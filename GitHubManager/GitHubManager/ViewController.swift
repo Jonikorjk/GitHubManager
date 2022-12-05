1747f18cd8648017f8c0880c91a5acc871ccf551
Jonik <zenaserdykov2004@gmail.com>
2022-12-05 13:47:33 +0200

    init

---
+++ GitHubManager/GitHubManager/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GitHubManager
+//
+//  Created by User on 05.12.2022.
+//
+
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
